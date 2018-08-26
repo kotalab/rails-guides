8d2c28bb08c435ddd821dd2b068d8d259ee7cdfd
kotalab <kotarolab@gmail.com>
2018-08-26 14:36:32 +0900

    2.ActiveRecord migrations. execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_121646) do
+ActiveRecord::Schema.define(version: 2018_08_26_052758) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,22 @@ ActiveRecord::Schema.define(version: 2018_08_21_121646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "customers_products", id: false, force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "part_number"
+    t.decimal "price"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
 end
